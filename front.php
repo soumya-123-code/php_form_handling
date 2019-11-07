6a458abd5c7f769afe8595c923d7b50a31072e65
soumyababu <39803577+soumyababu@users.noreply.github.com>
2019-11-07 09:37:15 +0530

    front.php

---
+++ front.php
@@ -0,0 +1,18 @@
+<?php
+echo $_POST['name'];
+echo $_POST['email'];
+
+
+?>
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
